43cab86f6229c5309bc363360e14bb07be60d8a4
josuecassab <josuecassab3@gmail.com>
2024-10-28 20:51:45 -0500

    Write to kafka

--- random_data_gen.py
+++ random_data_gen.py
@@ -4,21 +4,27 @@ from kafka.errors import KafkaError
 from json import dumps
 from random import randint
 import time
+import json
 
+
+with open("config.json") as file:
+    config = json.load(file)
+
+producer = KafkaProducer(
+    bootstrap_servers=[config['bootstrap.servers']],
+    security_protocol=config['security.protocol'],
+    sasl_mechanism=config['sasl.mechanism'],
+    sasl_plain_username=config['sasl.username'],
+    sasl_plain_password=config['sasl.password'],
+)
 
 fake = Faker()
 
-producer = KafkaProducer(
-    bootstrap_servers=['pkc-619z3.us-east1.gcp.confluent.cloud:9092'],
-    security_protocol='SASL_SSL',
-    sasl_mechanism='PLAIN',
-    sasl_plain_username='OMRL4EBMA2BQT762',
-    sasl_plain_password='349+1RWttT1QVINg45ep4G6SYG/iCVnM+6lC+EzKRAXhGwClsgdOXqFmzxStZWyb',
-)
 records = []
 
 for i in range(5):
     data = {
+        'id': fake.unique.random_int(min=10000, max=99999),
         'name': fake.name(),
         'email': fake.email(),
         'phone': fake.phone_number(),
@@ -35,15 +41,12 @@ for i in range(5):
         
         print(record)
         json_data = dumps(record).encode('utf-8')
-        future = producer.send('topic_0', json_data)
+        future = producer.send('source', json_data)
 
         try:
             record_metadata = future.get(timeout=10)
         except KafkaError as e:
             # Decide what to do if produce request failed...
             print(e)
-        
-        # Successful result returns assigned partition and offset
-        print (record_metadata.topic)
-        print (record_metadata.partition)
-        print (record_metadata.offset)
+            
+        time.sleep(5)
--- test.py
+++
@@ -1,5 +0,0 @@
-import time
-from datetime import datetime
-print(time.time())
-now = datetime.now()
-print(now, now.timestamp())
